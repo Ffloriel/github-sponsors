6f08a0d55b62831d119429a84e8cdfbc854f8b06
Ffloriel <florielfedry@gmail.com>
2019-05-24 22:24:55 +0200

    remove console log

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
-import program from "commander";
 import { parseFundingFile } from "./utils/parse";
 import { printDonationMessage } from "./utils/print";
 import { shouldHideMessage } from "./utils/misc";
 
 export async function init(path = process.cwd(), hideMessage = shouldHideMessage()) {
   if (hideMessage) return;
-  console.error('yo!')
   try {
     const fundingConfig = await parseFundingFile(path);
-    console.error('yo!!')
-    console.warn('can you see me?')
-    console.log('I hope you do :)')
     printDonationMessage(
       fundingConfig,
       path
